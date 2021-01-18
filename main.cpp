7f8fc281a2ac839e1ac24846ec2a78ae4575391e
MrTD16 <77585797+MrTD16@users.noreply.github.com>
2021-01-18 12:48:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main() {
     time_t timetoday;
     time (&timetoday);
 	//Hochkomplizierte Funktionalität
-    std::cout << "Hello World!";
+    std::cout << "Hello Mama!";
     std::cout << "Calendar date and time as per todays is : "<< asctime(localtime(&timetoday));
+    std::cout << "Thanh was here";
+    std::cout << "Olaf too";
     return 0;
 }
